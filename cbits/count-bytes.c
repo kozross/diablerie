cb890f90d721e917956965fcbdc80ad211833b08
Koz Ross <koz.ross@retro-freedom.nz>
2021-04-22 15:06:32 +1200

    SSE2 implementation of count_bytes_eq

--- cbits/count-bytes.c
+++ cbits/count-bytes.c
@@ -1,6 +1,6 @@
 #include <stdint.h> 
 #include <stddef.h> 
-#ifdef __AVX2__
+#if __AVX2__ && __POPCNT__
 #include <immintrin.h>
 
 int count_bytes_eq (uint8_t* ba, int off, int len, int w8) {
@@ -30,6 +30,65 @@ int count_bytes_eq (uint8_t* ba, int off, int len, int w8) {
     __m256i result4 = _mm256_cmpeq_epi8(input4, mask);
     int packed4 = _mm256_movemask_epi8(result4);
     total += __builtin_popcount(packed4);
+  }
+  for (size_t i = 0; i < small_steps; i++) {
+    if ((*ptr) == w8) {
+      total++;
+    }
+    ptr++;
+  }
+  return total;
+}
+#elif __SSE2__ && __POPCNT__
+#include <emmintrin.h>
+
+int count_bytes_eq (uint8_t* ba, int off, int len, int w8) {
+  int total = 0;
+  size_t big_steps = len / 128;
+  size_t small_steps = len % 128;
+  uint8_t* ptr = &(ba[off]);
+  __m128i mask = _mm_set1_epi8(w8);
+  for (size_t i = 0; i < big_steps; i++) {
+    __m128i input = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i input2 = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i input3 = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i input4 = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i input5 = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i input6 = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i input7 = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i input8 = _mm_loadu_si128((__m128i const*)ptr);
+    ptr += 16;
+    __m128i result = _mm_cmpeq_epi8(input, mask);
+    int packed = _mm_movemask_epi8(result);
+    total += __builtin_popcount(packed);
+    __m128i result2 = _mm_cmpeq_epi8(input2, mask);
+    int packed2 = _mm_movemask_epi8(result2);
+    total += __builtin_popcount(packed2);
+    __m128i result3 = _mm_cmpeq_epi8(input3, mask);
+    int packed3 = _mm_movemask_epi8(result3);
+    total += __builtin_popcount(packed3);
+    __m128i result4 = _mm_cmpeq_epi8(input4, mask);
+    int packed4 = _mm_movemask_epi8(result4);
+    total += __builtin_popcount(packed4);
+    __m128i result5 = _mm_cmpeq_epi8(input5, mask);
+    int packed5 = _mm_movemask_epi8(result5);
+    total += __builtin_popcount(packed5);
+    __m128i result6 = _mm_cmpeq_epi8(input6, mask);
+    int packed6 = _mm_movemask_epi8(result6);
+    total += __builtin_popcount(packed6);
+    __m128i result7 = _mm_cmpeq_epi8(input7, mask);
+    int packed7 = _mm_movemask_epi8(result7);
+    total += __builtin_popcount(packed7);
+    __m128i result8 = _mm_cmpeq_epi8(input8, mask);
+    int packed8 = _mm_movemask_epi8(result8);
+    total += __builtin_popcount(packed8);
   }
   for (size_t i = 0; i < small_steps; i++) {
     if ((*ptr) == w8) {
