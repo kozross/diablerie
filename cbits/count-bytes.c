96d9471544275cf134e3903399f72abac40b9c04
Koz Ross <koz.ross@retro-freedom.nz>
2021-06-20 18:17:22 +1200

    Clean up AVX and SSE implementations

--- cbits/count-bytes.c
+++ cbits/count-bytes.c
@@ -1,102 +1,145 @@
 #include <stdint.h> 
 #include <stddef.h> 
-#if __AVX2__ && !LINTERIEUR_LEGACY_SSE
-#include <immintrin.h>
 
-size_t count_bytes_eq (uint8_t* ba, size_t off, size_t len, int w8) {
-  __m256i totals = _mm256_setzero_si256();
-  __m256i mask = _mm256_set1_epi8(w8);
-  size_t big_steps = len / 256;
-  size_t small_steps = len % 256;
-  uint8_t* ptr = &(ba[off]);
-  for (size_t i = 0; i < big_steps; i++) {
-    __m256i input[8];
-    input[0] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    input[1] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    input[2] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    input[3] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    input[4] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    input[5] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    input[6] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    input[7] = 
-      _mm256_cmpeq_epi8(mask, _mm256_loadu_si256((__m256i const*)ptr));
-    ptr += 32;
-    __m256i summed = _mm256_add_epi8(_mm256_add_epi8(_mm256_add_epi8(input[0], input[1]),
-                                                     _mm256_add_epi8(input[2], input[3])),
-                                     _mm256_add_epi8(_mm256_add_epi8(input[4], input[5]),
-                                                     _mm256_add_epi8(input[6], input[7])));
-    totals = _mm256_add_epi64(totals, _mm256_sad_epu8(_mm256_abs_epi8(summed), 
-                                                      _mm256_setzero_si256()));
+static inline uint64_t broadcast (uint8_t byte) {
+  return byte * 0x0101010101010101ULL;
+}
+
+static size_t count_bytes_eq_fallback (uint8_t const * const src,
+                                       size_t const off,
+                                       size_t const len,
+                                       int const byte) {
+  size_t total = 0;
+  // We go a 64-bit word at a time.
+  size_t big_strides = len / 8;
+  size_t small_strides = len % 8;
+  uint8_t* ptr = (uint8_t*)&(src[off]);
+  // We use the method described in "Bit Twiddling Hacks".
+  // Source: https://graphics.stanford.edu/~seander/bithacks.html#ZeroInWord
+  uint64_t matches = broadcast(byte);
+  uint64_t mask = broadcast(0x7f);
+  for (size_t i = 0; i < big_strides; i++) {
+    uint64_t* big_ptr = (uint64_t*)ptr;
+    uint64_t input = (*big_ptr) ^ matches;
+    uint64_t tmp = (input & mask) + mask;
+    uint64_t result = ~(tmp | input | mask);
+    total += __builtin_popcountll(result);
+    ptr += 8;
   }
-  size_t total = _mm256_extract_epi64(totals, 0) + 
-              _mm256_extract_epi64(totals, 1) + 
-              _mm256_extract_epi64(totals, 2) +
-              _mm256_extract_epi64(totals, 3);
-  for (size_t i = 0; i < small_steps; i++) {
-    if ((*ptr) == w8) {
+  // Finish the rest the slow way.
+  for (size_t i = 0; i < small_strides; i++) {
+    if ((*ptr) == byte) {
       total++;
     }
     ptr++;
   }
   return total;
 }
-#elif __SSE4_1__
-#include <smmintrin.h>
+#if (__x86_64__ || __i386__)
+#include <emmintrin.h>
+#include <immintrin.h>
 
-int count_bytes_eq (uint8_t* ba, int off, int len, int w8) {
-  __m128i totals = _mm_setzero_si128();
-  __m128i mask = _mm_set1_epi8(w8);
-  size_t big_steps = len / 128;
-  size_t small_steps = len % 128;
-  uint8_t* ptr = &(ba[off]);
-  for (size_t i = 0; i < big_steps; i++) {
-    __m128i input[8];
-    input[0] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    input[1] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    input[2] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    input[3] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    input[4] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    input[5] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    input[6] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    input[7] = _mm_cmpeq_epi8(mask, _mm_loadu_si128((__m128i const*)ptr));
-    ptr += 16;
-    __m128i summed = _mm_add_epi8(_mm_add_epi8(_mm_add_epi8(input[0], input[1]),
-                                               _mm_add_epi8(input[2], input[3])),
-                                  _mm_add_epi8(_mm_add_epi8(input[4], input[5]),
-                                               _mm_add_epi8(input[6], input[7])));
-    totals = _mm_add_epi64(totals, _mm_sad_epu8(_mm_abs_epi8(summed),
-                                                _mm_setzero_si128()));
+static size_t count_bytes_eq_avx2 (uint8_t const * const src,
+                                   size_t const off,
+                                   size_t const len,
+                                   int const byte) {
+  // Keep four counters at once.
+  // This reduces the need to evacuate SIMD registers during bulk work.
+  __m256i totals = _mm256_setzero_si256();
+  __m256i matches = _mm256_set1_epi8(byte);
+  // Our stride is 8 SIMD registers at once.
+  // That's 32 bytes times 8 = 256.
+  size_t big_strides = len / 256;
+  size_t small_strides = len % 256;
+  uint8_t* ptr = (uint8_t*)&(src[off]);
+  // Big strides first, using our SIMD accumulators.
+  for (size_t i = 0; i < big_strides; i++) {
+    __m256i acc = _mm256_setzero_si256();
+    #pragma GCC unroll 8
+    for (size_t j = 0; j < 8; j++) {
+      __m256i input = _mm256_loadu_si256((__m256i*)ptr);
+      ptr += 32;
+      // This gives 0xFF in the lane on a match, which is -1. Thus, as we
+      // accumulate, we end up with between 0 and -8 in every accumulator lane.
+      acc = _mm256_add_epi8(acc, _mm256_cmpeq_epi8(matches, input));
+    }
+    // Stuff the accumulator into our counters, after taking the absolute value.
+    totals = _mm256_add_epi64(totals, 
+                              _mm256_sad_epu8(_mm256_abs_epi8(acc), 
+                                              _mm256_setzero_si256()));
   }
-  int total = _mm_extract_epi64(totals, 0) + 
-              _mm_extract_epi64(totals, 1);
-  for (size_t i = 0; i < small_steps; i++) {
-    if ((*ptr) == w8) {
+  // Evacuate our counters.
+  size_t total = _mm256_extract_epi64(totals, 0) + 
+                 _mm256_extract_epi64(totals, 1) +
+                 _mm256_extract_epi64(totals, 2) +
+                 _mm256_extract_epi64(totals, 3);
+  // Finish the job the slow way.
+  for (size_t i = 0; i < small_strides; i++) {
+    if ((*ptr) == byte) {
       total++;
     }
     ptr++;
   }
   return total;
+}
+
+static size_t count_bytes_eq_sse2 (uint8_t const * const src,
+                                   size_t const off,
+                                   size_t const len,
+                                   int const byte) {
+  // Keep two counters at once.
+  // This reduces the need to evacuate SIMD registers during bulk work.
+  __m128i totals = _mm_setzero_si128();
+  __m128i matches = _mm_set1_epi8(byte);
+  // Our stride is 8 SIMD registers at once.
+  // That's 16 bytes times 8 = 128.
+  size_t big_strides = len / 128;
+  size_t small_strides = len % 128;
+  uint8_t* ptr = (uint8_t*)&(src[off]);
+  // Big strides first, using our SIMD accumulators.
+  for (size_t i = 0; i < big_strides; i++) {
+    __m128i acc = _mm_setzero_si128();
+    #pragma GCC unroll 8
+    for (size_t j = 0; j < 8; j++) {
+      __m128i input = _mm_loadu_si128((__m128i*)ptr);
+      ptr += 32;
+      // This gives 0xFF in the line on a match, which is -1. Thus, as we
+      // accumulate, we end up with between 0 and -8 in every accumulator lane.
+      acc = _mm_add_epi8(acc, _mm_cmpeq_epi8(matches, input));
+    }
+    totals = _mm_add_epi64(totals,
+                           _mm_sad_epu8(_mm_abs_epi8(acc),
+                                        _mm_setzero_si128()));
+  }
+  // Evacuate our counters
+  __m128i lows = _mm_unpacklo_epi64(totals, totals);
+  __m128i highs = _mm_unpackhi_epi64(totals, totals);
+  __m128i counted = _mm_add_epi64(lows, highs);
+  size_t total = (size_t)_mm_movepi64_pi64(counted);
+  // Finish the job the slow way.
+  for (size_t i = 0; i < small_strides; i++) {
+    if ((*ptr) == byte) {
+      total++;
+    }
+    ptr++;
+  }
+  return total;
+}
+
+size_t count_bytes_eq (uint8_t const * const src,
+                       size_t const off,
+                       size_t const len,
+                       int const byte) {
+  __builtin_cpu_init();
+  if (__builtin_cpu_supports("avx2")) {
+    return count_bytes_eq_avx2(src, off, len, byte);
+  }
+  else if (__builtin_cpu_supports("sse2")) {
+    return count_bytes_eq_sse2(src, off, len, byte);
+  }
+  else {
+    return count_bytes_eq_fallback(src, off, len, byte);
+  }
 }
 #elif __ARM_NEON
 #include <arm_neon.h>
@@ -130,7 +173,7 @@ size_t count_bytes_eq (uint8_t const * const src,
   }
   // Evacuate our SIMD counters.
   size_t total = vpaddd_u64(totals);
-  // Tongue-crawl anything left.
+  // Finish the job one byte at a time.
   for (size_t i = 0; i < small_strides; i++) {
     if ((*ptr) == byte) {
       total++;
@@ -140,31 +183,7 @@ size_t count_bytes_eq (uint8_t const * const src,
   return total;
 }
 #else
-static uint64_t broadcast (uint8_t w8) {
-  return w8 * 0x0101010101010101ULL;
-}
-
-size_t count_bytes_eq (uint8_t* ba, size_t off, size_t len, int w8) {
-  size_t total = 0;
-  size_t big_steps = len / 8;
-  size_t small_steps = len % 8;
-  uint8_t* ptr = &(ba[off]);
-  uint64_t mask = broadcast(w8);
-  uint64_t mask2 = 0x7f7f7f7f7f7f7f7fULL;
-  for (size_t i = 0; i < big_steps; i++) {
-    uint64_t* big_ptr = (uint64_t*)ptr;
-    uint64_t input = (*big_ptr) ^ mask;
-    uint64_t tmp = (input & mask2) + mask2;
-    uint64_t result = ~(tmp | input | mask2);
-    total += __builtin_popcountll(result);
-    ptr += 8;
-  }
-  for (size_t i = 0; i < small_steps; i++) {
-    if ((*ptr) == w8) {
-      total++;
-    }
-    ptr++;
-  }
-  return total;
+size_t count_bytes_eq (uint8_t* src, size_t off, size_t len, int byte) {
+  return count_bytes_eq_fallback(src, off, len, byte);
 }
 #endif
